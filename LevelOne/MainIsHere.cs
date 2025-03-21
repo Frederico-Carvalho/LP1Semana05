b6a8921a054c20bc23b558f5f01e942ce2c5a619
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-03-21 16:09:51 +0000

    New method docgit add MainIsHere.cs

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -6,7 +6,7 @@ namespace LevelOne
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Amazing!");
+            Console.WriteLine("doc!");
         }
     }
 }
